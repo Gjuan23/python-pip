35cb6de5ce277b4f65798da5bb0792a526a149d8
Juan Gonzalez <gonzalezjuan1518@gmail.com>
2023-04-14 18:11:09 -0300

    add files

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 def run():
     return 'hola mi campeon'
+
+
+names = ['Juan','Nadia','Candela','Valentina']
+age = [26,25,4,3]
+
+new_dict = {k : v for k, v in zip(names, age)}
+
+
 if __name__ == '__main__':
     print(run())
+    print(new_dict)
+
+
+
